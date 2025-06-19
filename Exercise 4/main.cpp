6c22a0f614c30bc5bdaf78a1d37afd99d7840a84
Logan.Schejbal <loganschejbal@gmail.com>
2025-06-19 15:45:18 -0500

    Set Up of Exercise 4

---
+++ Exercise 4/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+//Function Definitions
+void sort2(double* p, double* q);
+double replace_if_greater(double* p, double x);
+double average(double* a, int size);
+double* maximum(double* a, int size);
+char* find(char s[], char c);
+char* find_last(char s[], char c);
+char* find_last(char s[], char c, int n);
+char* find(char s[], char t[]);
+char* replace_all(const char s[], const char t[], const char u[]);
+void reverse(double* a, int size);
+
+//Main Function
+int main() {
+
+}
+
+// Function Implementations
+//Make x <= y after the call
+//Write a function void sort2(double* p, double* q) that receives two pointers and sorts the values to which they point. If you call sort2(&x, &y) then x <= y after the call.
+void sort2(double* p, double* q){
+
+}
+
+//
+//Write a function double replace_if_greater(double* p, double x) that replaces the value to which p points with x if x is greater. Return the old value to which p pointed.
+double replace_if_greater(double* p, double x){
+
+}
+
+//
+//Write a function that computes the average value of an array of floating-point data: double average(double* a, int size) In the function, use a pointer variable, not an integer index, to traverse the array elements.
+double average(double* a, int size){
+
+}
+
+//
+//Write a function that returns a pointer to the maximum value of an array of floating-point data: double* maximum(double* a, int size) If size is 0, return nullptr.
+double* maximum(double* a, int size){
+
+}
+
+//
+//Write a function that returns a pointer to the first occurrence of the character c in the string s, or nullptr if there is no match. char* find(char s[], char c)
+char* find(char s[], char c){
+
+}
+
+//
+//Write a function that returns a pointer to the last occurrence of the character c in the string s, or nullptr if there is no match. char* find_last(char s[], char c)
+char* find_last(char s[], char c){
+
+}
+
+//
+//Write a function that returns a pointer to the nth occurrence of the character c in the string s, or nullptr if there is no match. char* find_last(char s[], char c, int n)
+char* find_last(char s[], char c, int n){
+
+}
+
+//
+//Write a function that returns a pointer to the first occurrence of the substring t in the string s, or nullptr if there is no match. char* find(char s[], char t[])
+char* find(char s[], char t[]){
+
+}
+
+//
+//Write a function that, given strings s, t, and u, returns a string (allocated with the new operator) in which all occurrences of t in s are replaced with u: char* replace_all(const char s[], const char t[], const char u[])
+char* replace_all(const char s[], const char t[], const char u[]){
+
+}
+
+//
+//Write a function that reverses the values of an array of floating-point data: void reverse(double* a, int size) In the function, use two pointer variables, not integer indexes, to traverse the array elements.
+void reverse(double* a, int size){
+
+}
